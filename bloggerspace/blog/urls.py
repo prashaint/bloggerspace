d6fb829517111a4c39e646e356b1086f15f65e6e
prashaint <prashaint_mishra@outlook.com>
2021-06-17 17:07:32 +0530

    Added Django Basic structure for blog website

---
+++ bloggerspace/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.start_page, name="start_page"),
+    path("posts", views.posts, name="posts"),
+    path("posts/<slug:slug>", views.post_detail, name="post_detail")
+]
+
