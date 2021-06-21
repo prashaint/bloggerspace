90d7473d1481a8bf01185f2549b602deb8d213f9
prashaint <prashaint_mishra@outlook.com>
2021-06-22 01:14:43 +0530

    Added Models and Admin panel stuffs

--- bloggerspace/blog/models.py
+++ bloggerspace/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.db.models.deletion import SET_NULL
+from django.db.models.fields import CharField
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, null=True, on_delete=SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
