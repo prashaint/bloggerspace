ad80396c5461587352f784caec332ef2f63b2c26
prashaint <prashaint_mishra@outlook.com>
2021-07-18 19:07:44 +0530

    Added style changes

--- bloggerspace/blog/admin.py
+++ bloggerspace/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, Author, Post
+from .models import Comment, Tag, Author, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,10 +7,12 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author", )
     prepopulated_fields = {"slug": ("title", )}
 
-
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
 
 # Register your models here.
 
 admin.site.register(Tag)
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
